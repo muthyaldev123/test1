65c8da735fc16e6433a3401df1261ae41a873698
muthyaldev123 <pulikantimuthyalreddy@gmail.com>
2024-08-25 20:23:10 +0530

    workshop

---
+++ index.js
@@ -0,0 +1,97 @@
+// let arr=["hello","world","abc"];
+// // let arr1=["xyz","abc","def"]
+// res=[]
+// function outp(x){
+//     for(a of x){
+//         for(i=0;i<a.length;i++){
+//             b=a.substring(0,3)
+//         }
+//         res.push(b)
+//     }
+//     console.log(res)
+// }
+// outp(arr) 
+
+
+
+
+// let arr=["hello","world","abc","foo"]
+// res=[]
+// function output(b){
+//     for(i=0;i<b.length;i++){
+//         c=b[i].replace("o","0")
+//         res.push(c)
+//     }
+//     console.log(res)
+// }
+// output(arr) 
+
+
+
+
+
+// let arr1=["hello","world","abc"]
+// let arr2=["xyz","abc","def"]
+// let res1=arr1.map((x)=>{return x.toUpperCase()})
+// let res2=arr2.map((z)=>{return z.toUpperCase()})
+// console.log(res1)
+// console.log(res2)  
+
+
+
+
+
+// function split(){
+//     let arr=["hello-world","abc-def"]
+//     let a=arr.map((x)=>x.split(`-`))
+//     console.log(a)
+// }
+// split()
+
+// function filer(){
+// let arr=["hello","world","abc","defghi"]
+// let arr1=["xyz","abcdef"]
+// let y=arr1.filter(a=>a.length>3)
+// let x=arr.filter((a)=>a.length>5)
+// console.log(y)
+// console.log(x)
+// }
+// filer()
+
+
+//     let arr=["hello","world","abc"]
+//     let x=arr.map(rock)
+//     function rock(a){
+//    return a.toUpperCase()+(`!`)
+
+//     }
+//     console.log(x)
+
+  
+
+// let arr=["hello","world","abc"]
+// let x=arr.find((z)=>{return z==="world"})
+// console.log(x)
+
+
+// let arr=["hello","world","abc"]
+// let x=arr.find(rock)
+// function rock(a){
+//     return a.find,a==="world"
+// }
+// console.log(x)
+
+
+// let arr=["hello","world","abc"]
+// let x=arr.join(rock)
+// function rock(a){
+// return a.
+// }
+// console.log(x) 
+
+let arr=["hello","world","abc"]
+let arr2=["xyz","abc"]
+let arr1=arr.join(",");
+let arr3=arr2.join(",");
+console.log(arr1)
+console.log(arr3)
